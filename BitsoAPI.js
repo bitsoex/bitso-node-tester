4b2db95cd832617b8c809074e96b394b49bed8dc
Rodrigo Reyes <rodrigo@plethora.com.mx>
2019-10-09 00:02:21 -0500

    Added testAvailableBooks and testTicker

--- BitsoAPI.js
+++ BitsoAPI.js
@@ -1,26 +1,36 @@
 const crypto = require("crypto");
 const https = require("axios");
-const buildQueryString = require('build-query-string');
+const buildQueryString = require("build-query-string");
 
 class BitsoAPI {
-    constructor(BITSO_API_KEY, BITSO_API_SECRET, BASE_URL, URL_PREFIX="") {
-     this.BITSO_API_KEY = BITSO_API_KEY;
-     this.BITSO_API_SECRET = BITSO_API_SECRET;
-     this.BASE_URL = BASE_URL;
-     this.URL_PREFIX = URL_PREFIX;
+    constructor(BITSO_API_KEY, BITSO_API_SECRET, BASE_URL, URL_PREFIX = "") {
+        this.BITSO_API_KEY = BITSO_API_KEY;
+        this.BITSO_API_SECRET = BITSO_API_SECRET;
+        this.BASE_URL = BASE_URL;
+        this.URL_PREFIX = URL_PREFIX;
     }
     async getBalances() {
         return this.privateRequest("GET", "/balance");
     }
-    async withdrawCrypto(currency, amount, address, max_fee=null, destination_tag=null) {
+    async withdrawCrypto(
+        currency,
+        amount,
+        address,
+        max_fee = null,
+        destination_tag = null
+    ) {
         const withdrawalPayload = {
             currency,
             amount,
             address,
             max_fee,
             destination_tag
-        }
-        return this.privateRequest("POST", "/crypto_withdrawal", withdrawalPayload);
+        };
+        return this.privateRequest(
+            "POST",
+            "/crypto_withdrawal",
+            withdrawalPayload
+        );
     }
 
     async placeOrder(book, side, amount, type) {
@@ -29,33 +39,35 @@ class BitsoAPI {
             side,
             type,
             major: amount
-        }
+        };
         return this.privateRequest("POST", "/orders", orderPayload);
     }
-    async placeSellOrder(book, amount, type="market") {
+    async placeSellOrder(book, amount, type = "market") {
         return this.placeOrder(book, "sell", amount, type);
     }
     /*
     */
-    async placeBuyOrder(book, amount, type="market") {
+    async placeBuyOrder(book, amount, type = "market") {
         return this.placeOrder(book, "buy", amount, type);
     }
 
-    async privateRequest(method, url, payload="", params = null){
-        let options = {};
-        options.method = method;
-        options.url = this.URL_PREFIX+url;
+    async getAvailableBooks() {
+        return this.publicRequest("GET", "/available_books");
+    }
+
+    async getCurrentTicker(book) {
+        return this.publicRequest("GET", "/ticker", { book });
+    }
+
+    async privateRequest(method, url, payload = "", params = null) {
+        let options = this.basicRequestForge(method, url, params);
         options.headers = {
             "Content-Type": "application/json"
         };
-        options.baseURL = this.BASE_URL;
-        if (method == "GET" && params !== null) {
-            option.url += buildQueryString(params);
-        }
         let nonce = new Date().getTime();
         let stringified_payload = "";
         if (payload !== "") {
-            stringified_payload = JSON.stringify(payload)
+            stringified_payload = JSON.stringify(payload);
             options.data = payload;
         }
         let hmac = crypto.createHmac("sha256", this.BITSO_API_SECRET);
@@ -64,22 +76,30 @@ class BitsoAPI {
         );
         let hexed_signature = hmac.digest("hex");
         hmac.end();
-        let authorization = `Bitso ${this.BITSO_API_KEY}:${nonce}:${hexed_signature}`;
+        let authorization = `Bitso ${
+            this.BITSO_API_KEY
+        }:${nonce}:${hexed_signature}`;
         options.headers.Authorization = authorization;
         let req = await https.request(options);
         if (req.data) return req.data;
         else return (ctx.body = "ERROR");
     }
-    async publicRequest(method, url, params){
-        options.method = method;
-        options.url = this.URL_PREFIX+url;
-        options.baseURL = this.BASE_URL;
-        if (method == "GET" && params !== null) {
-            option.url += buildQueryString(params);
-        }
-        let req = await https.request(options);
+    async publicRequest(method, url, params = null) {
+        let req = await https.request(
+            this.basicRequestForge(method, url, params)
+        );
         if (req.data) return req.data;
         else return (ctx.body = "ERROR");
+    }
+    basicRequestForge(method, url, params = null) {
+        let options = {};
+        options.method = method;
+        options.url = this.URL_PREFIX + url;
+        options.baseURL = this.BASE_URL;
+        if (method == "GET" && params !== null) {
+            options.url += `?${buildQueryString(params)}`;
+        }
+        return options;
     }
 }
 module.exports = BitsoAPI;
--- test.js
+++ test.js
@@ -1,67 +1,97 @@
 const bitso = require("./BitsoAPI");
-const chalk = require('chalk');
+const chalk = require("chalk");
 const BITSO_URL = process.env.BITSO_URL;
 const BITSO_API_KEY = process.env.BITSO_API_KEY;
 const BITSO_API_SECRET = process.env.BITSO_API_SECRET;
 
-const bitsoAPI = new bitso(BITSO_API_KEY, BITSO_API_SECRET, BITSO_URL, "/api/v3")
+const bitsoAPI = new bitso(
+    BITSO_API_KEY,
+    BITSO_API_SECRET,
+    BITSO_URL,
+    "/api/v3"
+);
 
-    function displayError(Exception) {
-        console.log(chalk.red("[FAILED]"))
-        if(Exception.response!== undefined) {
-            console.log(chalk.red(`${Exception.response.status} : ${Exception.response.statusText} ${JSON.stringify(Exception.response.data)}`));
-        } else {
-            console.log(Exception);
-        }
+function displayError(Exception) {
+    console.log(chalk.red("[FAILED]"));
+    if (Exception.response !== undefined) {
+        console.log(
+            chalk.red(
+                `${Exception.response.status} : ${
+                    Exception.response.statusText
+                } ${JSON.stringify(Exception.response.data)}`
+            )
+        );
+    } else {
+        console.log(Exception);
     }
-    function checkIfSuccessful(test, data) {
-        if(data !== undefined && data.success) {
-            console.log(chalk.green(`${test} TEST ✓`))
-        } else {
-            console.log(chalk.red(`${test} TEST X`))
-            console.log(chalk.red(JSON.stringify(data)));
-        }
-        return data.success;
+}
+function checkIfSuccessful(test, data) {
+    if (data !== undefined && data.success) {
+        console.log(chalk.green(`${test} TEST ✓`));
+    } else {
+        console.log(chalk.red(`${test} TEST X`));
+        console.log(chalk.red(JSON.stringify(data)));
     }
-    async function testBalances() {
-        preTestingMessage("BALANCES");
-        try {
-            let response = await bitsoAPI.getBalances();
-            if(checkIfSuccessful("BALANCES", response))
-                return response.payload;
-        }catch(Exception){
-            displayError(Exception)
-        }
+    return data.success;
+}
+async function testBalances() {
+    preTestingMessage("BALANCES");
+    try {
+        let response = await bitsoAPI.getBalances();
+        if (checkIfSuccessful("BALANCES", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
     }
-    async function testWithdrawals(currency, amount, address) {
-        preTestingMessage("WITHDRAWALS", currency, amount, address);
-        try {
-            let response = await bitsoAPI.withdrawCrypto(currency, amount, address);
-            if(checkIfSuccessful("WITHDRAWALS", response))
-                return response.payload;
-        }catch(Exception){
-            displayError(Exception)
-        }
+}
+async function testWithdrawals(currency, amount, address) {
+    preTestingMessage("WITHDRAWALS", currency, amount, address);
+    try {
+        let response = await bitsoAPI.withdrawCrypto(currency, amount, address);
+        if (checkIfSuccessful("WITHDRAWALS", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
     }
-    async function testOrders(book, side, amount, type) {
-        preTestingMessage("ORDERS", book, side, amount, type);
-        try {
-            let response = await bitsoAPI.placeOrder(book, side, amount, type);
-            if(checkIfSuccessful("ORDERS", response))
-                return response.payload;
-        }catch(Exception){
-            displayError(Exception)
-        }
+}
+async function testOrders(book, side, amount, type) {
+    preTestingMessage("ORDERS", book, side, amount, type);
+    try {
+        let response = await bitsoAPI.placeOrder(book, side, amount, type);
+        if (checkIfSuccessful("ORDERS", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
     }
+}
+async function testAvailableBooks() {
+    preTestingMessage("AVAILABLE BOOKS");
+    try {
+        let response = await bitsoAPI.getAvailableBooks();
+        if (checkIfSuccessful("AVAILABLE BOOKS", response))
+            return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
 
-    function preTestingMessage(test, ...rest) {
-        console.log(chalk.blue(`TESTING ${test} ENDPOINT`));
-        console.log(chalk.blue(`USING PARAMETERS: ${rest}`));
+async function testTicker(book) {
+    preTestingMessage("TICKER", book);
+    try {
+        let response = await bitsoAPI.getCurrentTicker(book);
+        if (checkIfSuccessful("TICKER", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
     }
+}
+
+function preTestingMessage(test, ...rest) {
+    console.log(chalk.blue(`TESTING ${test} ENDPOINT`));
+    console.log(chalk.blue(`USING PARAMETERS: ${rest}`));
+}
 
 // Simply tests the functions no actual integrity checks
 async function basicTesting() {
-    console.log(chalk.white("INITIALIZING BASIC TESTING"))
+    console.log(chalk.white("INITIALIZING BASIC TESTING"));
+    await testAvailableBooks();
+    await testTicker("btc_mxn");
     await testOrders("eth_mxn", "buy", "1.0", "market");
     await testOrders("eth_mxn", "sell", "1.0", "market");
     await testWithdrawals("btc", "0.001", "15YB8xZ4GhHCHRZXvgmSFAzEiDosbkDyoo");
@@ -69,6 +99,3 @@ async function basicTesting() {
 }
 
 basicTesting();
-  
- 
-
